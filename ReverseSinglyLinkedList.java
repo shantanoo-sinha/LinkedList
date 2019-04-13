f8a0103a592e6de168f8802691f8ff390692794e
Shantanoo Sinha <shantanoo.sinha@gmail.com>
2019-04-13 21:52:19 +0530

    Update ReverseSinglyLinkedList.java

--- ReverseSinglyLinkedList.java
+++ ReverseSinglyLinkedList.java
@@ -1,1 +1,14 @@
 
+public void reverse(Node head) {
+  Node previous = null;
+  Node current = head;
+  Node following = head;
+  
+  while (current != null) {
+    following = following.next;
+    current.next = previous;
+    previous = current;
+    current = following;
+  }
+  return previous:
+}
